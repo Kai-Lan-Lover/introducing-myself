3898ad3ea91f5bddeb12f96f030febbcc89648a5
Kai-Lan-Lover <heerrajpal15@gmail.com>
2024-12-06 17:31:23 +0530

    New changes

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 basic.showString("Hi" + "guys")
+record.startRecording(record.BlockingState.Blocking)
 basic.showArrow(ArrowNames.North)
 basic.showArrow(ArrowNames.NorthEast)
 basic.showArrow(ArrowNames.East)
 basic.showArrow(ArrowNames.SouthEast)
 basic.showArrow(ArrowNames.West)
 basic.showArrow(ArrowNames.NorthWest)
-record.startRecording(record.BlockingState.Blocking)
 basic.showString("I" + "am" + "Heer" + "Rajpal" + "btw!!")
 record.startRecording(record.BlockingState.Blocking)
 basic.showLeds(`
